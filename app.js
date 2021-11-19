4535d0e1a5be106d681f102b3417493d06550227
akira <rafael.akkira@gmail.com>
2021-11-19 15:36:35 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-let numberOfSquare = 25;
-let squareWidth = numberOfSquare;
-let squreHeight = numberOfSquare;
-let canvasWidth = numberOfSquare * numberOfSquare;
-let canvasHeight = numberOfSquare;
+let squareDimension = 20
+let numberOfSquare = 30;
+let squareWidth = squareDimension;
+let squreHeight = squareDimension;
+let canvasWidth = squareDimension * numberOfSquare;
+let canvasHeight = squareDimension * numberOfSquare;
 
 
 let container = document.querySelector('.container');
 container.style.width = `${canvasWidth}px`;
+container.style.height = `${canvasHeight}px`;
 
 
 function generateCanvas() {
 	let canvas = document.querySelector('.canvas');
 
-	for(let i = 0; i < numberOfSquare; i++) {
-		for(let j = 0; j < numberOfSquare; j++) {
-			let canvasSquare = document.createElement("div");
-			canvasSquare.style.width = `${squareWidth}px`;
-			canvasSquare.style.height = `${squreHeight}px`;
-			canvasSquare.style.border = "1px solid #333";
+	for(let i = 0; i < numberOfSquare * numberOfSquare; i++) {
+		let canvasSquare = document.createElement("div");
+		canvasSquare.style.width = `${squareWidth}px`;
+		canvasSquare.style.height = `${squreHeight}px`;
+		canvasSquare.style.border = "1px solid #457b9d";
 
-			canvas.append(canvasSquare);
-		}
+		canvas.append(canvasSquare);
 	}
 }
 
@@ -36,19 +36,31 @@ function resetCanvas() {
 }
 
 function fillDivs(element) {
-	let canvas = document.querySelector('.canvas');
 
-	canvas.addEventListener('mousemove', (element) => {
-		console.log(element);
-	});
+	if (!element.target.style.backgroundColor) {
+		element.target.style.backgroundColor = "#963D5A";
+	}
+
+	// else  {
+	// 	element.target.style.removeProperty('background-color');
+	// }
 }
 
+function drawWhilstMoving(target, whileMove) {
+	let endMove = function(){
+		this.removeEventListener('mousemove', whileMove);
+	}
+
+	target.addEventListener('mousedown', function(element) {
+		element.stopPropagation();
+		target.addEventListener('mousemove', whileMove);
+		target.addEventListener('mouseup', endMove);
+	});
+}
 
 generateCanvas();
 
 let btnClear = document.querySelector('.btn-clear');
 btnClear.addEventListener('click', resetCanvas);
 
-// this code work filling the div background and works better than filling div with background color and seens like it's faster
-document.querySelector('.canvas').addEventListener('mousedown',  fillDivs);
-
+drawWhilstMoving(document.querySelector('.canvas'), fillDivs);
